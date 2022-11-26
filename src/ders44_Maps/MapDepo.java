ac1294a87daa543be79677407a9f97d243c67dba
omerkorkmaz01 <o.krkmz2004@gmail.com>
2022-11-26 11:48:56 +0300

    git commit -m "44.gun sonu"

--- src/ders44_Maps/MapDepo.java
+++ src/ders44_Maps/MapDepo.java
@@ -1,2 +1,54 @@
-package ders44_Maps;public class MapDepo {
+package ders44_Maps;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapDepo {
+
+    // ogrenci map olusturup bize donduren bir method olusturun
+
+    public static Map<Integer, String> ornekMapOlustur(){
+
+    Map<Integer, String> ogrenciMap= new HashMap<>();
+        ogrenciMap.put(101,"Ali-Can-10-H-MF");
+        ogrenciMap.put(102,"Veli-Cem-11-H-Soz");
+        ogrenciMap.put(103,"Ali-Cem-11-H-TM");
+        ogrenciMap.put(104,"Ayse-Can-10-H-MF");
+        ogrenciMap.put(105,"Ayse-Cem-11-H-TM");
+        ogrenciMap.put(106,"Fatma-Han-10-K-Soz");
+
+        return ogrenciMap;
+    }
+    public static String isimSoyisimDondur(Map<Integer, String> ogrenciMap, int ogrenciKey) {
+
+
+        String ogrenciValue= ogrenciMap.get(ogrenciKey); // "Ayse-Cem-11-H-TM"
+
+        String[] valueArr=ogrenciValue.split("-"); // [Ayse, Cem, 11, H, TM]
+
+        String isimSoyisim=valueArr[0]+" "+valueArr[1]; // Ayse Cem
+
+        return isimSoyisim;
+
+    }
+
+    public static Map<Integer, String> ogrenciyeYeniSubeAta(Map<Integer, String> ogrenciMap, int ogrenciKey,String yeniSube) {
+        // ogrenci key map'de var mı?
+
+        if (ogrenciMap.containsKey(ogrenciKey)) {
+            // istenen Key varsa, update edip yeni map'i dondurecegiz
+            String istenenKeyinValuesu= ogrenciMap.get(ogrenciKey); // Ayse-Can-10-H-MF
+            String [] istenenKeyValueArr= istenenKeyinValuesu.split("-"); // [Ayse, Can, 10, H, Mf]
+            // yeni sube degeri atayalim
+            istenenKeyValueArr[3]=yeniSube; // [Ayse, Can, 10, K, Mf]
+
+            String istenenKeyinYeniValuesu= istenenKeyValueArr[0]+"-"+
+                                            istenenKeyValueArr[1]+"-"+
+                                            istenenKeyValueArr[2]+"-"+
+                                            istenenKeyValueArr[3]+"-"+
+                                            istenenKeyValueArr[4];
+            ogrenciMap.put(ogrenciKey,istenenKeyinYeniValuesu);
+        }
+        return ogrenciMap;
+    }
 }
